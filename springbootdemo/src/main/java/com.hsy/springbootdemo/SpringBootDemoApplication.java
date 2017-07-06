f345441d3701f664eecc25cc1d7b91defe49627f
shiyuan2he <shiyuan4work@sina.com>
2017-07-07 07:36:53 +0800

    ssl 秘钥

--- springbootdemo/src/main/java/com.hsy/springbootdemo/SpringBootDemoApplication.java
+++ springbootdemo/src/main/java/com.hsy/springbootdemo/SpringBootDemoApplication.java
@@ -4,6 +4,10 @@ import com.hsy.springbootdemo.javabean.protocolbean.Author;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerInitializedEvent;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,5 +33,17 @@ public class SpringBootDemoApplication {
     }
     public static void main(String[] args){
         SpringApplication.run(SpringBootDemoApplication.class,args);
+    }
+
+    @Bean
+    public EmbeddedServletContainerFactory servletContainer(){
+        TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory(){
+
+            protected void postProcessContext(){
+
+            }
+        } ;
+
+        return  null ;
     }
 }
