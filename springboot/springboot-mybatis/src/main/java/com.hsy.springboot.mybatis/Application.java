71e25e76e31761ce8bccf484a77c375f1e4364d4
shiyuan2he <shiyuan4work@sina.com>
2017-09-12 11:25:08 +0800

    build

---
+++ springboot/springboot-mybatis/src/main/java/com.hsy.springboot.mybatis/Application.java
@@ -0,0 +1,21 @@
+package com.hsy.springboot.mybatis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author heshiyuan
+ * @description <p></p>
+ * @path springboot/com.hsy.springboot.mybatis
+ * @date 12/09/2017 10:31 AM
+ * @github http://github.com/shiyuan2he
+ * @email shiyuan4work@sina.com
+ * Copyright (c) 2017 shiyuan4work@sina.com All rights reserved.
+ * @price ¥5    微信：hewei1109
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args){
+        SpringApplication.run(Application.class,args) ;
+    }
+}
