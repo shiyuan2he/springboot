f483a503b6991e8d2b602a37dadab586a08c52be
shiyuan2he <shiyuan4work@sina.com>
2017-12-04 15:28:07 +0800

    springboot整合servlet

--- springboot-servlet/src/main/java/com/hsy/springboot/servlet/SpringBootServletApplication.java
+++ springboot-servlet/src/main/java/com/hsy/springboot/servlet/SpringBootServletApplication.java
@@ -1,4 +1,12 @@
 package com.hsy.springboot.servlet;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+@SpringBootApplication
+@ServletComponentScan(basePackages = "com.hsy.springboot.servlet")
 public class SpringBootServletApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootServletApplication.class,args);
+    }
 }
--- springboot-servlet/src/main/java/com/hsy/springboot/servlet/config/ServertConfig.java
+++ springboot-servlet/src/main/java/com/hsy/springboot/servlet/config/ServertConfig.java
@@ -1,4 +1,29 @@
 package com.hsy.springboot.servlet.config;
 
+import com.hsy.springboot.servlet.servlet.MyServlet;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.DispatcherServlet;
+
+@Configuration
 public class ServertConfig {
+    /**
+     * 方法一：通过ServletRegistrationBean注册
+     * @return
+     */
+    @Bean
+    public ServletRegistrationBean servletRegistrationBean(){
+        return new ServletRegistrationBean(new MyServlet(),"/myServlet/*") ;
+    }
+
+    /*@Bean
+    public ServletRegistrationBean dispatcherRegistration(DispatcherServlet dispatcherServlet) {
+        ServletRegistrationBean registration = new ServletRegistrationBean(dispatcherServlet);
+        registration.getUrlMappings().clear();
+        registration.addUrlMappings("*.do");
+        registration.addUrlMappings("*.view");
+        registration.addUrlMappings("*.json");
+        return registration;
+    }*/
 }
