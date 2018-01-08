6f611bf9d17374cf02cb4d091990b4e87b648ce7
shiyuan2he <shiyuan4work@sina.com>
2018-01-08 18:57:25 +0800

    logback日志优化

--- springboot-logback/src/main/java/com/hsy/springboot/logback/LogbackApplication.java
+++ springboot-logback/src/main/java/com/hsy/springboot/logback/LogbackApplication.java
@@ -19,7 +19,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LogbackApplication {
     private final static Logger _logger = LoggerFactory.getLogger(LogbackApplication.class);
     public static void main(String[] args) {
-        _logger.info("this is springboot main");
+
         SpringApplication.run(LogbackApplication.class,args);
+        for (int i=0;i<10;i++){
+            _logger.info("this is springboot main");
+        }
     }
 }
