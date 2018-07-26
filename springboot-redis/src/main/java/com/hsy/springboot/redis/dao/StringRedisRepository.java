e2e0addf6461a73d375edbdd6734244b843bd3c9
shiyuan2he <shiyuan4work@sina.com>
2018-07-26 18:23:37 +0800

    springboot整合redis

--- springboot-redis/src/main/java/com/hsy/springboot/redis/dao/StringRedisRepository.java
+++ springboot-redis/src/main/java/com/hsy/springboot/redis/dao/StringRedisRepository.java
@@ -1,28 +1,23 @@
 package com.hsy.springboot.redis.dao;
 
-import com.hsy.java.util.cache.redis.impl.AbstractSpringRedisCacheEnhance;
+import com.hsy.java.util.cache.redis.impl.AbstractSpringStringRedisCacheEnhance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
-import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Repository;
 
-import javax.annotation.Resource;
-import java.util.concurrent.TimeUnit;
+@Repository(value = "stringRedisRepository")
+public class StringRedisRepository extends AbstractSpringStringRedisCacheEnhance {
 
-@Repository(value = "redisRepository")
-public class RedisRepository extends AbstractSpringRedisCacheEnhance{
-
-    @Autowired
-    RedisTemplate<String, Object> redisTemplate;
+    @Autowired StringRedisTemplate stringRedisTemplate;
 
     @Override
     public StringRedisTemplate getStringRedisTemplate() {
-        return null;
+        return stringRedisTemplate;
     }
 
     @Override
     public RedisTemplate<String, Object> getRedisTemplate() {
-        return redisTemplate;
+        return null;
     }
 }
