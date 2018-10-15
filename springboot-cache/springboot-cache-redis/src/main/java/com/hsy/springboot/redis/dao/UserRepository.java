747a2c3d65acd585b32befb0163468b43ac06296
shiyuan2he <shiyuan4work@126.com>
2018-10-15 23:17:36 +0800

    springboot集成redis初步整合

---
+++ springboot-cache/springboot-cache-redis/src/main/java/com/hsy/springboot/redis/dao/UserRepository.java
@@ -0,0 +1,15 @@
+package com.hsy.springboot.redis.dao;
+
+import com.hsy.springboot.redis.model.User;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.jpa.repository.JpaRepository;
+@CacheConfig(cacheNames = "users")
+public interface UserRepository extends JpaRepository<User, Long> {
+    @Cacheable(key = "#p0")
+    User findByName(String name);
+
+    @CachePut(key = "#p0.name")
+    User save(User user);
+}
---
+++ springboot-cache/springboot-cache-redis/src/main/java/com/hsy/springboot/redis/model/User.java
@@ -0,0 +1,57 @@
+package com.hsy.springboot.redis.model;
+
+import java.io.Serializable;
+
+/**
+ * @author heshiyuan
+ * @description <p></p>
+ * @path springboot/com.hsy.springboot.redis.model
+ * @date 2017/12/29 9:17
+ * @github http://github.com/shiyuan2he
+ * @email shiyuan4work@sina.com
+ * Copyright (c) 2017 shiyuan4work@sina.com All rights reserved.
+ * @price ¥5    微信：hewei1109
+ */
+public class User implements Serializable{
+
+    private Long id ;
+    private String name ;
+    private int age;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public User(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
